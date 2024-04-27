82cca12ca956e6334ead3cfa0fcc8d2027e2d18c
Cristian Narváez <cristianfns11@gmail.com>
2024-04-27 10:14:11 -0500

    Se empezó con el back end

---
+++ back_end/tuVooz/src/main/java/com/sena/tuVooz/model/usuario.java
@@ -0,0 +1,25 @@
+package com.sena.tuVooz.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name="usuario")
+public class usuario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id_usuario", nullable = false, length = 36)
+    private String id_usuario; 
+
+    @Column(name = "correo_electronico", nullable = false, length = 255)
+	private String correo_electronico;
+
+    @Column(name="nombre_usuario", nullable = false, length = 30)
+    private String nombre_usuario;
+
+    @Column(name="contrasena_usuario", nullable = false, length = 30)
+    private String contrasena_usuario;
+}
