e806f3b8357421e21d50bf9bb2083ed47fe9cc8b
Cristian Narváez <cristianfns11@gmail.com>
2024-04-28 12:59:26 -0500

    Hice el model de usuario

--- back_end/tuVooz/src/main/java/com/sena/tuVooz/model/usuario.java
+++ back_end/tuVooz/src/main/java/com/sena/tuVooz/model/usuario.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+
 @Entity(name="usuario")
 public class usuario {
     
@@ -23,6 +24,52 @@ public class usuario {
     @Column(name="contrasena_usuario", nullable = false, length = 30)
     private String contrasena_usuario;
 
+    public usuario() {
+		super();
+	}
+
+    public usuario(String id_usuario, String correo_electronico, String nombre_usuario, String contrasena_usuario )
+	{
+		super();
+		
+		this.id_usuario = id_usuario;
+		this.correo_electronico = correo_electronico;
+        this.nombre_usuario = nombre_usuario;
+        this.contrasena_usuario = contrasena_usuario;
+	}
+
+
+    public String getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(String id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public String getCorreo_electronico() {
+        return correo_electronico;
+    }
+
+    public void setCorreo_electronico(String correo_electronico) {
+        this.correo_electronico = correo_electronico;
+    }
+
+    public String getNombre_usuario() {
+        return nombre_usuario;
+    }
+
+    public void setNombre_usuario(String nombre_usuario) {
+        this.nombre_usuario = nombre_usuario;
+    }
+
+    public String getContrasena_usuario() {
+        return contrasena_usuario;
+    }
+
+    public void setContrasena_usuario(String contrasena_usuario) {
+        this.contrasena_usuario = contrasena_usuario;
+    }
 
     
 }
