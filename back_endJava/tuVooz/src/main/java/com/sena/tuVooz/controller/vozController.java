d3ecf1ba691147392676bcdb2b85067148a3493f
Cristian Narváez <cristianfns11@gmail.com>
2024-05-29 17:03:22 -0500

    Ya se usó la IA  de Google Cloud de Text to Speech. Quedó perfecta <3

--- back_endJava/tuVooz/src/main/java/com/sena/tuVooz/controller/vozController.java
+++ back_endJava/tuVooz/src/main/java/com/sena/tuVooz/controller/vozController.java
@@ -1,58 +1,14 @@
 package com.sena.tuVooz.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.google.api.gax.core.FixedCredentialsProvider;
-import com.google.auth.oauth2.ServiceAccountCredentials;
-import com.google.cloud.texttospeech.v1.*;
-import com.google.protobuf.ByteString;
 
-import java.io.FileInputStream;
 
 
 @RestController
 @RequestMapping("/api/v1/voz")
 public class vozController {
-    @GetMapping("/")
-    public String home() throws Exception  {
-     
-        // Path al archivo JSON de credenciales
-        String credFilePath = "C:\\Users\\SENA\\AppData\\Roaming\\gcloud\\application_default_credentials.json";
-
-        // Inicializar el cliente
-        try (FileInputStream credentialsStream = new FileInputStream(credFilePath)) {
-            TextToSpeechSettings settings = TextToSpeechSettings.newBuilder()
-                    .setCredentialsProvider(FixedCredentialsProvider.create(ServiceAccountCredentials.fromStream(credentialsStream)))
-                    .build();
-            try (TextToSpeechClient textToSpeechClient = TextToSpeechClient.create(settings)) {
-
-            // Configurar la solicitud de síntesis
-            SynthesisInput input = SynthesisInput.newBuilder()
-                    .setText("Hola, este es un ejemplo de texto a voz con Google Cloud Text-to-Speech.")
-                    .build();
-
-            VoiceSelectionParams voice = VoiceSelectionParams.newBuilder()
-                    .setLanguageCode("es-ES") // Elige el idioma adecuado
-                    .setSsmlGender(SsmlVoiceGender.FEMALE) // Elige el género adecuado
-                    .build();
-
-            AudioConfig audioConfig = AudioConfig.newBuilder()
-                    .setAudioEncoding(AudioEncoding.LINEAR16)
-                    .build();
-
-            // Realizar la solicitud de síntesis
-            SynthesizeSpeechResponse response = textToSpeechClient.synthesizeSpeech(input, voice, audioConfig);
-
-            // Escribir la respuesta a un archivo de audio
-            ByteString audioContents = response.getAudioContent();
-            try (java.io.FileOutputStream out = new java.io.FileOutputStream("output.wav")) {
-                out.write(audioContents.toByteArray());
-            }
-            System.out.println("Audio content written to file \"output.wav\"");
-        }
-    }
-}
+    
 }
