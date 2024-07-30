42776471760a9a925602ce60be97b6bca3df42fb
Angie Trujillo <lizethtrujillogonzalez11@gmail.com>
2024-07-30 16:57:00 -0500

    Cambios de nosotros

--- backend/tuvooz/viewAI.py
+++ backend/tuvooz/viewAI.py
@@ -20,7 +20,7 @@ def synthesize(request):
 
             client = texttospeech.TextToSpeechClient()
             input_text = texttospeech.SynthesisInput(text=text)
-            voice = texttospeech.VoiceSelectionParams(language_code="es-US", name="es-US-Standard-B")
+            voice = texttospeech.VoiceSelectionParams(language_code="es-US", name="es-US-Wavenet-A")
             audio_config = texttospeech.AudioConfig(audio_encoding=texttospeech.AudioEncoding.MP3)
             response = client.synthesize_speech(input=input_text, voice=voice, audio_config=audio_config)
 
