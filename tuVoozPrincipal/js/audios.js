457c71ebe17a51e1b7a6858ba6e4e66eee43357b
unknown <mairatp2005@gmail.com>
2024-09-05 17:13:55 -0500

    Actualizacion de tarjetsas

---
+++ tuVoozPrincipal/js/audios.js
@@ -0,0 +1,208 @@
+//<<<<<<<<<<<<<< Inicio audio-Saludos-Despedidas <<<<<<<<<<<<<<<<<<<<<
+var soundHola = new Audio(); 
+soundHola.src = "./sonido/Hola.wav"; 
+
+var soundBuenosDias = new Audio(); 
+soundBuenosDias.src = "./sonido/Buenos_dias.wav"; 
+
+var soundBuenasTardes = new Audio(); 
+soundBuenasTardes.src = "./sonido/Buenas_tardes.wav"; 
+
+var soundBuenasNoches = new Audio(); 
+soundBuenasNoches.src = "./sonido/Buenas_noches.wav"; 
+
+var soundEncantadoDeConocerte = new Audio(); 
+soundEncantadoDeConocerte.src = "./sonido/Encantado_de_conocerte.wav"; 
+
+var soundQueGustoVerte = new Audio(); 
+soundQueGustoVerte.src = "./sonido/Que_gusto_verte.wav"; 
+
+var soundEsUnPlacerConocerte = new Audio(); 
+soundEsUnPlacerConocerte.src = "./sonido/Es_un_placer_conocerte.wav"; 
+
+var soundAdios = new Audio(); 
+soundAdios.src = "./sonido/Adios.wav"; 
+
+var soundHastaPronto = new Audio(); 
+soundHastaPronto.src = "./sonido/Hasta_pronto.wav"; 
+
+var soundHastaLuego = new Audio(); 
+soundHastaLuego.src = "./sonido/Hasta_luego.wav"; 
+
+var soundHastaMañana = new Audio(); 
+soundHastaMañana.src = "./sonido/Hasta_mañana.wav"; 
+
+var soundNosVemos = new Audio(); 
+soundNosVemos.src = "./sonido/Nos_vemos.wav"; 
+
+var soundChao = new Audio(); 
+soundChao.src = "./sonido/Chao.wav";
+
+//<<<<<<<<<<<<<< Fin audio-Saludos-Despedidas <<<<<<<<<<<<<<<<<<<<<
+
+//<<<<<<<<<<<<<< Inicio Emociones <<<<<<<<<<<<<<<<<<<<<
+var soundFeliz = new Audio(); 
+soundFeliz.src = "./sonido/Feliz.wav"; 
+
+var soundSorprendido = new Audio(); 
+soundSorprendido.src = "./sonido/Sorprendido.wav"; 
+
+var soundConfundido = new Audio(); 
+soundConfundido.src = "./sonido/Confundido.wav"; 
+
+var soundEnojado = new Audio(); 
+soundEnojado.src = "./sonido/Enojado.wav"; 
+
+var sounEstresado = new Audio(); 
+sounEstresado.src = "./sonido/Estresado.wav"; 
+
+var soundTranquilo = new Audio(); 
+soundTranquilo.src = "./sonido/Tranquilo.wav"; 
+
+var soundTriste = new Audio(); 
+soundTriste.src = "./sonido/Triste.wav"; 
+
+var soundAburrido = new Audio(); 
+soundAburrido.src = "./sonido/Aburrido.wav"; 
+
+var soundEnamorado = new Audio(); 
+soundEnamorado.src = "./sonido/Enamorado.wav"; 
+
+var soundMalo = new Audio(); 
+soundMalo.src = "./sonido/Malo.wav"; 
+
+var soundEnfermo = new Audio(); 
+soundEnfermo.src = "./sonido/Enfermo.wav"; 
+
+//<<<<<<<<<<<<<< Fin Emociones <<<<<<<<<<<<<<<<<<<<<
+
+//<<<<<<<<<<<<<< Inicio Animales <<<<<<<<<<<<<<<<<<<
+
+var soundPerro = new Audio(); 
+soundPerro.src = "./sonido/Perro.wav"; 
+
+var soundGato = new Audio(); 
+soundGato.src = "./sonido/Gato.wav"; 
+
+var soundElefante = new Audio(); 
+soundElefante.src = "./sonido/Elefante.wav"; 
+
+var soundLeon = new Audio(); 
+soundLeon.src = "./sonido/León.wav"; 
+
+var soundTigre = new Audio(); 
+soundTigre.src = "./sonido/Tigre.wav"; 
+
+var soundOso = new Audio(); 
+soundOso.src = "./sonido/Oso.wav"; 
+
+var soundCaballo = new Audio(); 
+soundCaballo.src = "./sonido/Caballo.wav"; 
+
+var soundDelfin = new Audio(); 
+soundDelfin.src = "./sonido/Delfín.wav"; 
+
+var soundAguila = new Audio(); 
+soundAguila.src = "./sonido/Aguila.wav"; 
+
+var soundPinguino = new Audio(); 
+soundPinguino.src = "./sonido/Pinguino.wav"; 
+
+var soundCebra = new Audio(); 
+soundCebra.src = "./sonido/Cebra.wav"; 
+
+var soundJirafa = new Audio(); 
+soundJirafa.src = "./sonido/Jirafa.wav"; 
+
+var soundLobo = new Audio(); 
+soundLobo.src = "./sonido/Lobo.wav"; 
+
+var soundKoala = new Audio(); 
+soundKoala.src = "./sonido/Koala.wav"; 
+
+var soundPanda = new Audio(); 
+soundPanda.src = "./sonido/Panda.wav"; 
+
+var soundLoro = new Audio(); 
+soundLoro.src = "./sonido/Loro.wav"; 
+
+var soundTortuga = new Audio(); 
+soundTortuga.src = "./sonido/Tortuga.wav"; 
+
+var soundPato = new Audio(); 
+soundPato.src = "./sonido/Pato.wav"; 
+
+var soundGallina = new Audio(); 
+soundGallina.src = "./sonido/Gallina.wav"; 
+
+var soundPollo = new Audio(); 
+soundPollo.src = "./sonido/Pollo.wav"; 
+
+var soundPez = new Audio(); 
+soundPez.src = "./sonido/Pez.wav"; 
+
+//<<<<<<<<<<<<<< Fin animales <<<<<<<<<<<<<<<<<<<<
+
+//<<<<<<<<<<<<<<Inicio Preguntas <<<<<<<<<<<<<<<<<<<<
+
+var sound = new Audio(); 
+sound.src = "./sonido/Pez.wav";
+
+var sound = new Audio(); 
+sound.src = "./sonido/Pez.wav";
+
+var sound = new Audio(); 
+sound.src = "./sonido/Pez.wav";
+
+var sound = new Audio(); 
+sound.src = "./sonido/Pez.wav";
+
+var sound = new Audio(); 
+sound.src = "./sonido/Pez.wav";
+
+var sound = new Audio(); 
+sound.src = "./sonido/Pez.wav";
+
+var sound = new Audio(); 
+sound.src = "./sonido/Pez.wav";
+
+var sound = new Audio(); 
+sound.src = "./sonido/Pez.wav";
+
+var sound = new Audio(); 
+sound.src = "./sonido/Pez.wav";
+
+var sound = new Audio(); 
+sound.src = "./sonido/Pez.wav";
+
+var sound = new Audio(); 
+sound.src = "./sonido/Pez.wav";
+
+var sound = new Audio(); 
+sound.src = "./sonido/Pez.wav";
+
+var sound = new Audio(); 
+sound.src = "./sonido/Pez.wav";
+
+var sound = new Audio(); 
+sound.src = "./sonido/Pez.wav";
+
+var sound = new Audio(); 
+sound.src = "./sonido/Pez.wav";
+
+var sound = new Audio(); 
+sound.src = "./sonido/Pez.wav";
+
+var sound = new Audio(); 
+sound.src = "./sonido/Pez.wav";
+
+var sound = new Audio(); 
+sound.src = "./sonido/Pez.wav";
+
+var sound = new Audio(); 
+sound.src = "./sonido/Pez.wav";
+
+
+
+
+
