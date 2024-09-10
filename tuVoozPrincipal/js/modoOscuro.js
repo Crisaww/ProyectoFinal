afa0d68c6fa7f9d29081ee8f20fb4f32b77864d2
Angie Trujillo <lizethtrujillogonzalez11@gmail.com>
2024-09-10 12:58:56 -0500

    modo oscuro

--- tuVoozPrincipal/js/modoOscuro.js
+++ tuVoozPrincipal/js/modoOscuro.js
@@ -4,16 +4,15 @@ function modoOscuro(){
     root.classList.add('oscuro');
     localStorage.setItem('modo', 'oscuro');
     root.style.setProperty('--background-color', '#000000');
-    root.style.setProperty('--h1-color','#ffffff')
-    root.style.setProperty('--h2-color','#ffffff')
+    root.style.setProperty('--h1-color','#b91414');
     // root.style.setProperty('--color-p')
 }
 function modoClaro(){
     root.classList.remove('oscuro');
     localStorage.setItem('modo', 'claro');
     root.style.setProperty('--background-color', '#ffffff');
-    root.style.setProperty('--h1-color','#000000')
-    root.style.setProperty('--h1-color','#000000')
+    root.style.setProperty('--h1-color','#000000');
+
 
     
 }
