da4fc9f87f7acd9f7091d314d45ec341235d04b3
Carolina <carolina22cmc@gmail.com>
2024-09-11 16:19:51 -0500

    agregue cambios en config y setting

--- tuVoozPrincipal/js/cuenta/config.js
+++ tuVoozPrincipal/js/cuenta/config.js
@@ -1,5 +1,4 @@
 //let urlBasica= "http://127.0.0.1:8000/"
-let urlBasica="http://5.183.11.147:8000/";//ip servidor
 //let urlBasica="http://192.168.17.3:8000/";
 let urlLogin= urlBasica+"tuvooz/api/v1/iniciarSesion";
 let urlRegistro=urlBasica+"tuvooz/api/v1/registro";
@@ -163,25 +162,10 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function logout() {
-    Swal.fire({
-        title: "Advertencia",
-        text: "¿Estás seguro de que quieres cerrar sesión?",
-        icon: "warning",
-        showCancelButton: true, // Botón para cancelar la acción
-        confirmButtonText: "Sí, cerrar sesión",
-        cancelButtonText: "Cancelar",
-    }).then((result) => {
-        if (result.isConfirmed) {
-            // Si el usuario confirma, procede a cerrar sesión
-            localStorage.removeItem('access_token');
-            localStorage.removeItem('refresh_token');
-            Swal.fire("Sesión cerrada", "Has cerrado sesión correctamente.", "success").then(() => {
-                window.location.href = "http://127.0.0.1:5502/tuVoozPrincipal/cuenta/iniciarSesion.html";
-            });
-        }
-    });
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('refresh_token');
+    window.location.href = "http://127.0.0.1:5502/tuVoozPrincipal/cuenta/iniciarSesion.html";
 }
-
 
 
 
