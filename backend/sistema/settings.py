ebc3dd836f1bc6f12af566e7c1ba3fbc329b1e8f
Cristian Narváez <cristianfns11@gmail.com>
2024-09-11 12:53:07 -0500

    eliminé body de style.css

--- backend/sistema/settings.py
+++ backend/sistema/settings.py
@@ -111,8 +111,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'tuvoozdb',
         'USER': 'root',
-        'PASSWORD': '123456',
-        #'PASSWORD': 'root123',# se debe cambiar esto de acuerdo a la DB que van a utilizar
+        #'PASSWORD': '123456',
+        'PASSWORD': 'root123',# se debe cambiar esto de acuerdo a la DB que van a utilizar
         'HOST': 'localhost',  # o la dirección IP de tu servidor MySQL
         'PORT': '3306',       
     }
