67454ed4164cdbd4d3160d14f085b9d81b2b9e80
Maria Del Mar <mariadelmar.artunduaga911@gmail.com>
2024-09-29 10:32:32 -0500

    cambie la funcion obtener token en config

--- TuVooz/tuVoozPrincipal/js/cuenta/config.js
+++ TuVooz/tuVoozPrincipal/js/cuenta/config.js
@@ -17,25 +17,10 @@ let urlObtenerToken = urlBasica + "api/token/";
 let urlCambiarContrasenna = urlBasica + "tuvooz/api/v1/cambiarContrasenna/";
 let urlInicioSesion = urlBasicaFront +"TuVooz/tuVoozPrincipal/cuenta/iniciarSesion.html";
 
-async function obtenerTokens(username, password) {
-    try {
-        const response = await fetch(urlObtenerToken, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ username, password })
-        });
-        const data = await response.json();
-        if (response.ok) {
-            localStorage.setItem('access_token', data.access);
-            localStorage.setItem('refresh_token', data.refresh);
-            return true;
-        } else {
-            throw new Error(data.detail || 'Error al obtener el token');
-        }
-    } catch (error) {
-        console.error('Error:', error);
-        return false;
-    }
+function obtenerTokens() {
+    const access_token = localStorage.getItem('access_token');
+    const refresh_token = localStorage.getItem('refresh_token');
+    return { access_token, refresh_token };
 }
 
 async function logout() {
