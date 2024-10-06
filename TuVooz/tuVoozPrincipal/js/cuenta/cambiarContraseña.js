33ba61ccf085254aa70c1765b7af0293f84dbb0d
Cristian Narváez <cristianfns11@gmail.com>
2024-10-06 00:04:23 -0500

    Acomodé los tippy para que sean responsivos

--- TuVooz/tuVoozPrincipal/js/cuenta/cambiarContraseña.js
+++ TuVooz/tuVoozPrincipal/js/cuenta/cambiarContraseña.js
@@ -1,215 +1,218 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const toggleIcons = document.querySelectorAll('.grupo-contrasena i');
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleIcons = document.querySelectorAll(".grupo-contrasena i");
 
-    toggleIcons.forEach(icon => {
-        icon.addEventListener('click', function() {
-            const passwordInput = this.previousElementSibling; // Input que está antes del icono
+  toggleIcons.forEach((icon) => {
+    icon.addEventListener("click", function () {
+      const passwordInput = this.previousElementSibling; // Input que está antes del icono
 
-            // Verifica que el input de contraseña existe
-            if (passwordInput && passwordInput.type === 'password') {
-                passwordInput.type = 'text';
-                icon.classList.remove('fa-eye');
-                icon.classList.add('fa-eye-slash');
-            } else if (passwordInput && passwordInput.type === 'text') {
-                passwordInput.type = 'password';
-                icon.classList.remove('fa-eye-slash');
-                icon.classList.add('fa-eye');
-            }
-        });
+      // Verifica que el input de contraseña existe
+      if (passwordInput && passwordInput.type === "password") {
+        passwordInput.type = "text";
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+      } else if (passwordInput && passwordInput.type === "text") {
+        passwordInput.type = "password";
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+      }
     });
+  });
 });
+
 // Función de validación de contraseña
 function validarPassword(password) {
     let valor = password.value.trim();
     let valido = true;
     let mensajeError = "";
-
+  
+    // Detecta el tamaño de la pantalla y ajusta la colocación del tooltip
+    let placement = window.matchMedia("(max-width: 767px)").matches ? 'top' : 'right';
+  
     let tippyInstancePassword = tippy(password, {
-        content: '',
-        trigger: 'manual',
-        placement: 'right',
-        theme: 'material',
+      content: "",
+      trigger: "manual",
+      placement: placement,  // Cambia la colocación dependiendo del tamaño de pantalla
+      theme: "material",
     });
-
+  
     if (valor.length < 8 || valor.length > 20) {
-        valido = false;
-        mensajeError = "La contraseña debe tener entre 8 y 20 caracteres.";
+      valido = false;
+      mensajeError = "La contraseña debe tener entre 8 y 20 caracteres.";
     } else if (!/[A-Z]/.test(valor)) {
-        valido = false;
-        mensajeError = "La contraseña debe tener al menos una letra mayúscula.";
+      valido = false;
+      mensajeError = "La contraseña debe tener al menos una letra mayúscula.";
     } else if (!/[a-z]/.test(valor)) {
-        valido = false;
-        mensajeError = "La contraseña debe tener al menos una letra minúscula.";
+      valido = false;
+      mensajeError = "La contraseña debe tener al menos una letra minúscula.";
     } else if (!/[0-9]/.test(valor)) {
-        valido = false;
-        mensajeError = "La contraseña debe tener al menos un número.";
+      valido = false;
+      mensajeError = "La contraseña debe tener al menos un número.";
     } else if (!/[!@#$%^&*(),.?":{}|<>]/.test(valor)) {
-        valido = false;
-        mensajeError = "La contraseña debe tener al menos un carácter especial.";
+      valido = false;
+      mensajeError = "La contraseña debe tener al menos un carácter especial.";
     }
-
+  
     if (!valido) {
-        password.className = "form-control is-invalid";
-        tippyInstancePassword.setContent(mensajeError);
-        tippyInstancePassword.show();
+      password.className = "form-control is-invalid";
+      tippyInstancePassword.setContent(mensajeError);
+      tippyInstancePassword.show();
     } else {
-        password.className = "form-control is-valid";
-        tippyInstancePassword.hide();
+      password.className = "form-control is-valid";
+      tippyInstancePassword.hide();
     }
-
+  
     return valido;
-}
-
-// Agregar event listeners para validación en tiempo real
-document.getElementById('nuevaContrasena').addEventListener('input', function() {
+  }
+  
+  // Agregar event listeners para validación en tiempo real
+  document.getElementById("nuevaContrasena").addEventListener("input", function () {
     validarPassword(this);
-});
-
-document.getElementById('confirmarContrasena').addEventListener('input', function() {
-    const nuevaContrasena = document.getElementById('nuevaContrasena');
-    
+  });
+  
+  document.getElementById("confirmarContrasena").addEventListener("input", function () {
+    const nuevaContrasena = document.getElementById("nuevaContrasena");
+  
     if (this.value !== nuevaContrasena.value) {
-        this.className = "form-control is-invalid";
-        if (this._tippy) {  // Verifica si _tippy existe
-            this._tippy.setContent('Las contraseñas no coinciden');
-            this._tippy.show();
-        }
+      this.className = "form-control is-invalid";
+      if (this._tippy) {
+        this._tippy.setContent("Las contraseñas no coinciden");
+        this._tippy.show();
+      }
     } else {
-        this.className = "form-control is-valid";
-        if (this._tippy) {
-            this._tippy.hide();
-        }
+      this.className = "form-control is-valid";
+      if (this._tippy) {
+        this._tippy.hide();
+      }
     }
-});
-
-// Inicializar Tippy para los campos de contraseña
-tippy('#nuevaContrasena', {
-    content: 'Ingrese la nueva contraseña',
-    placement: 'right',
-    trigger: 'manual'
-});
-
-tippy('#confirmarContrasena', {
-    content: 'Confirme la nueva contraseña',
-    placement: 'right',
-    trigger: 'manual'
-});
-
-async function cambiarContrasena() {
-    const currentPasswordInput = document.getElementById('contrasenaActual');
-    const newPasswordInput = document.getElementById('nuevaContrasena');
-    const confirmPasswordInput = document.getElementById('confirmarContrasena');
-
+  });
+  
+  // Inicializar Tippy para los campos de contraseña
+  tippy("#nuevaContrasena", {
+    content: "Ingrese la nueva contraseña",
+    placement: window.matchMedia("(max-width: 767px)").matches ? 'top' : 'right',
+    trigger: "manual",
+  });
+  
+  tippy("#confirmarContrasena", {
+    content: "Confirme la nueva contraseña",
+    placement: window.matchMedia("(max-width: 767px)").matches ? 'top' : 'right',
+    trigger: "manual",
+  });
+  
+  // Función para cambiar la contraseña
+  async function cambiarContrasena() {
+    const currentPasswordInput = document.getElementById("contrasenaActual");
+    const newPasswordInput = document.getElementById("nuevaContrasena");
+    const confirmPasswordInput = document.getElementById("confirmarContrasena");
+  
     // Validar que se haya ingresado la contraseña actual
     if (!currentPasswordInput.value.trim()) {
-        Swal.fire({
-            icon: 'error',
-            title: 'Error',
-            text: 'Por favor, ingrese su contraseña actual'
-        });
-        return;
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "Por favor, ingrese su contraseña actual",
+      });
+      return;
     }
-
+  
     // Validar la nueva contraseña
     if (!validarPassword(newPasswordInput)) {
-        return;
+      return;
     }
-
+  
     // Validar que ambas contraseñas nuevas coincidan
     if (newPasswordInput.value !== confirmPasswordInput.value) {
-        Swal.fire({
-            icon: 'error',
-            title: 'Error',
-            text: 'Las contraseñas nuevas no coinciden'
-        });
-        return;
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "Las contraseñas nuevas no coinciden",
+      });
+      return;
     }
-
+  
     try {
-        const response = await fetch(urlCambiarContrasenna, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + localStorage.getItem('access_token')
-            },
-            body: JSON.stringify({
-                current_password: currentPasswordInput.value,
-                new_password: newPasswordInput.value,
-                confirm_password: confirmPasswordInput.value
-            })
-        });
-
-        const data = await response.json();
-
-        if (response.ok) {
-            Swal.fire({
-                icon: 'success',
-                title: '¡Éxito!',
-                text: 'Contraseña cambiada exitosamente'
-            }).then(() => {
-                // se eliminan los tokens y se cierra la sesion
-                localStorage.removeItem('access_token');
-                localStorage.removeItem('refresh_token');
-
-               // Redireccionar al usuario a la página de inicio de sesión
-                window.location.href = urlInicioSesion;
-            });
-    
-            if (data.access) localStorage.setItem('access_token', data.access);
-            if (data.refresh) localStorage.setItem('refresh_token', data.refresh);
-        } else {
-            throw new Error(data.error || 'Error al cambiar la contraseña');
-        }
-    } catch (error) {
+      const response = await fetch(urlCambiarContrasenna, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + localStorage.getItem("access_token"),
+        },
+        body: JSON.stringify({
+          current_password: currentPasswordInput.value,
+          new_password: newPasswordInput.value,
+          confirm_password: confirmPasswordInput.value,
+        }),
+      });
+  
+      const data = await response.json();
+  
+      if (response.ok) {
         Swal.fire({
-            icon: 'error',
-            title: 'Error',
-            text: error.message
+          icon: "success",
+          title: "¡Éxito!",
+          text: "Contraseña cambiada exitosamente",
+        }).then(() => {
+          // Se eliminan los tokens y se cierra la sesión
+          localStorage.removeItem("access_token");
+          localStorage.removeItem("refresh_token");
+  
+          // Redireccionar al usuario a la página de inicio de sesión
+          window.location.href = urlInicioSesion;
         });
+  
+        if (data.access) localStorage.setItem("access_token", data.access);
+        if (data.refresh) localStorage.setItem("refresh_token", data.refresh);
+      } else {
+        throw new Error(data.error || "Error al cambiar la contraseña");
+      }
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: error.message,
+      });
     }
-}
+  }
+  
 // Función para obtener el token JWT
 async function obtenerToken(username, password) {
-    try {
-        const response = await fetch(urlObtenerToken, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ username, password })
-        });
-        const data = await response.json();
-        if (response.ok) {
-            localStorage.setItem('access_token', data.access);
-            localStorage.setItem('refresh_token', data.refresh);
-            return true;
-        } else {
-            throw new Error(data.detail || 'Error al obtener el token');
-        }
-    } catch (error) {
-        console.error('Error:', error);
-        return false;
+  try {
+    const response = await fetch(urlObtenerToken, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password }),
+    });
+    const data = await response.json();
+    if (response.ok) {
+      localStorage.setItem("access_token", data.access);
+      localStorage.setItem("refresh_token", data.refresh);
+      return true;
+    } else {
+      throw new Error(data.detail || "Error al obtener el token");
     }
+  } catch (error) {
+    console.error("Error:", error);
+    return false;
+  }
 }
-
-
 
 // Función para refrescar el token
 async function refreshToken() {
-    try {
-        const response = await fetch(urlRefrescarToken, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ refresh: localStorage.getItem('refresh_token') })
-        });
-        const data = await response.json();
-        if (response.ok) {
-            localStorage.setItem('access_token', data.access);
-            return true;
-        } else {
-            throw new Error('No se pudo refrescar el token');
-        }
-    } catch (error) {
-        console.error('Error al refrescar el token:', error);
-        return false;
+  try {
+    const response = await fetch(urlRefrescarToken, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ refresh: localStorage.getItem("refresh_token") }),
+    });
+    const data = await response.json();
+    if (response.ok) {
+      localStorage.setItem("access_token", data.access);
+      return true;
+    } else {
+      throw new Error("No se pudo refrescar el token");
     }
+  } catch (error) {
+    console.error("Error al refrescar el token:", error);
+    return false;
+  }
 }
-
--- TuVooz/tuVoozPrincipal/js/cuenta/perfil.js
+++ TuVooz/tuVoozPrincipal/js/cuenta/perfil.js
@@ -1,168 +1,166 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Función para obtener el perfil del usuario
+  async function obtenerPerfil() {
+    try {
+      const token = localStorage.getItem("access_token");
+      if (!token) {
+        throw new Error("Token no encontrado en el localStorage");
+      }
 
-document.addEventListener('DOMContentLoaded', function() {
-    // Función para obtener el perfil del usuario
-    async function obtenerPerfil() {
-        try {
-            const token = localStorage.getItem('access_token');
-            if (!token) {
-                throw new Error('Token no encontrado en el localStorage');
-            }
-    
-            const response = await fetch(urlPerfil, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}`
-                }
-            });
-    
-            if (!response.ok) {
-                if (response.status === 401) {
-                    // Token expirado o no válido, redirigir al login
-                    window.location.href = urlInicioSesion;
-                }
-                throw new Error('Error al obtener el perfil');
-            }
-    
-            const data = await response.json();
-            document.getElementById('username').value = data.username;
-        } catch (error) {
-            Swal.fire({
-                title: 'Error',
-                text: error.message,
-                icon: 'error',
-                confirmButtonText: 'Aceptar'
-            });
+      const response = await fetch(urlPerfil, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (!response.ok) {
+        if (response.status === 401) {
+          // Token expirado o no válido, redirigir al login
+          window.location.href = urlInicioSesion;
         }
-    }
-    
-    
-    // Función para validar el nombre de usuario
-function validarUsername(usernameInput) {
-    let tippyInstanceUsername = tippy(usernameInput, {
-        content: '',
-        trigger: 'manual',
-        placement: 'right',
-        theme: 'material',
-    });
+        throw new Error("Error al obtener el perfil");
+      }
 
+      const data = await response.json();
+      document.getElementById("username").value = data.username;
+    } catch (error) {
+      Swal.fire({
+        title: "Error",
+        text: error.message,
+        icon: "error",
+        confirmButtonText: "Aceptar",
+      });
+    }
+  }
+
+  // Función para validar el nombre de usuario
+function validarUsername(usernameInput) {
+    // Detecta el tamaño de la pantalla y ajusta la colocación del tooltip
+    let placement = window.matchMedia("(max-width: 1023px)").matches ? 'top' : 'right';
+  
+    let tippyInstanceUsername = tippy(usernameInput, {
+      content: "",
+      trigger: "manual",
+      placement: placement,  // Usa la colocación en función del tamaño de la pantalla
+      theme: "material",
+    });
+  
     let valor = usernameInput.value.trim();
     let valido = true;
     let mensajesError = [];
-
+  
     // Verifica si el nombre está vacío
     if (!valor) {
-        usernameInput.className = "form-control is-invalid";
-        tippyInstanceUsername.setContent('El nombre de usuario es obligatorio.');
-        tippyInstanceUsername.show();
-        return false;
+      usernameInput.className = "form-control is-invalid";
+      tippyInstanceUsername.setContent("El nombre de usuario es obligatorio.");
+      tippyInstanceUsername.show();
+      return false;
     }
-
-     // Verifica la longitud mínima (al menos 3 caracteres)
-     if (valor.length < 3) {
-        valido = false;
-        mensajesError.push("El nombre de usuario debe contener al menos 3 caracteres.");
+  
+    // Verifica la longitud mínima (al menos 3 caracteres)
+    if (valor.length < 3) {
+      valido = false;
+      mensajesError.push("El nombre de usuario debe contener al menos 3 caracteres.");
     }
-
+  
     // Verifica la longitud del nombre
     if (valor.length > 50) { // Ajusta la longitud máxima según sea necesario
-        valido = false;
-        mensajesError.push("El nombre de usuario debe tener hasta 50 caracteres.");
+      valido = false;
+      mensajesError.push("El nombre de usuario debe tener hasta 50 caracteres.");
     }
-
+  
     // Verifica si el nombre contiene espacios
     if (/\s/.test(valor)) {
-        valido = false;
-        mensajesError.push("El nombre de usuario no debe contener espacios.");
+      valido = false;
+      mensajesError.push("El nombre de usuario no debe contener espacios.");
     }
-
+  
     // Verifica si el nombre contiene caracteres especiales
     if (/[^a-zA-Z0-9]/.test(valor)) {
-        valido = false;
-        // Solo añade el mensaje de caracteres especiales si ya no hay otros errores
-        if (!mensajesError.some(msg => msg.includes("espacios"))) {
-            mensajesError.push("El nombre de usuario no debe contener caracteres especiales.");
-        }
+      valido = false;
+      // Solo añade el mensaje de caracteres especiales si ya no hay otros errores
+      if (!mensajesError.some((msg) => msg.includes("espacios"))) {
+        mensajesError.push("El nombre de usuario no debe contener caracteres especiales.");
+      }
     }
-
+  
     if (!valido) {
-        usernameInput.className = "form-control is-invalid";
-        tippyInstanceUsername.setContent(mensajesError.join(' '));
-        tippyInstanceUsername.show();
+      usernameInput.className = "form-control is-invalid";
+      tippyInstanceUsername.setContent(mensajesError.join(" "));
+      tippyInstanceUsername.show();
     } else {
-        usernameInput.className = "form-control is-valid";
-        tippyInstanceUsername.hide();
+      usernameInput.className = "form-control is-valid";
+      tippyInstanceUsername.hide();
     }
-
+  
     return valido;
-}
+  }
+  
 
-// Función para actualizar el nombre de usuario
-async function actualizarUsername() {
+  // Función para actualizar el nombre de usuario
+  async function actualizarUsername() {
     try {
-        const usernameInput = document.getElementById('new-username');
+      const usernameInput = document.getElementById("new-username");
 
-        // Validar nombre de usuario antes de enviar la solicitud
-        if (!validarUsername(usernameInput)) {
-            return;
-        }
+      // Validar nombre de usuario antes de enviar la solicitud
+      if (!validarUsername(usernameInput)) {
+        return;
+      }
 
-        const token = localStorage.getItem('access_token');
-        if (!token) {
-            throw new Error('Token no encontrado en el localStorage');
-        }
+      const token = localStorage.getItem("access_token");
+      if (!token) {
+        throw new Error("Token no encontrado en el localStorage");
+      }
 
-        const nuevoUsername = usernameInput.value;
+      const nuevoUsername = usernameInput.value;
 
-        const response = await fetch(urlPerfil, {
-            method: 'PATCH',  // Cambia el método si es necesario para tu API
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            },
-            body: JSON.stringify({
-                'username': nuevoUsername
-            })
-        });
+      const response = await fetch(urlPerfil, {
+        method: "PATCH", // Cambia el método si es necesario para tu API
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          username: nuevoUsername,
+        }),
+      });
 
-        if (!response.ok) {
-            const errorData = await response.json();
-            throw new Error(errorData.error || 'Error al actualizar el nombre de usuario');
-        }
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(
+          errorData.error || "Error al actualizar el nombre de usuario"
+        );
+      }
 
-        Swal.fire({
-            title: 'Éxito',
-            text: 'Nombre de usuario actualizado correctamente',
-            icon: 'success',
-            confirmButtonText: 'Aceptar'
-        }).then(() => {
-            window.location.reload();
-        });
-
+      Swal.fire({
+        title: "Éxito",
+        text: "Nombre de usuario actualizado correctamente",
+        icon: "success",
+        confirmButtonText: "Aceptar",
+      }).then(() => {
+        window.location.reload();
+      });
     } catch (error) {
-        Swal.fire({
-            title: 'Error',
-            text: 'No se pudo actualizar el nombre de usuario',
-            icon: 'error',
-            confirmButtonText: 'Aceptar'
-        });
+      Swal.fire({
+        title: "Error",
+        text: "No se pudo actualizar el nombre de usuario",
+        icon: "error",
+        confirmButtonText: "Aceptar",
+      });
     }
-}
+  }
 
+  // Obtener perfil cuando se cargue la página
+  obtenerPerfil();
 
-
-
-
-
-    // Obtener perfil cuando se cargue la página
-    obtenerPerfil();
-
-    // Evento para actualizar el nombre de usuario al hacer clic en el botón correspondiente
-    const btnActualizarUsername = document.getElementById('btnActualizarUsername');
-    btnActualizarUsername.addEventListener('click', function(event) {
-        event.preventDefault();
-        actualizarUsername();
-    });
-
+  // Evento para actualizar el nombre de usuario al hacer clic en el botón correspondiente
+  const btnActualizarUsername = document.getElementById(
+    "btnActualizarUsername"
+  );
+  btnActualizarUsername.addEventListener("click", function (event) {
+    event.preventDefault();
+    actualizarUsername();
+  });
 });
-
